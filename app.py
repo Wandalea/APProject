d2636bef6f66bf0f01866a641f855f9e99f06d3f
Wandalea Upshall <wandaleau@gmail.com>
2026-04-12 22:41:50 -0230

    Add category routes for organizing crystal types

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import flask, render_template, request, redirect, url_for, session
+from werkzeug.utils import secure_filename
+import os
+
+
+@app.route("categories")
+def categories():
+    crystalTypes = [
+        {
+            "name": "Raw Crystals",
+            "description": "Natural and unpolished crystals",
+            "image" : "rawCrystals.jpg"
+        },
+        {
+            "name": "Tumbled Crystals",
+            "description": "Smooth polished pocket crystals",
+            "image": "tumbledCrystals.jpg"
+        },
+        {
+            "name": "Crystal Towers",
+            "description": "Pointed standing crystal pieces",
+            "image": "towerCrystals.jpg"
+        },
+        {
+            "name": "Carved Crystals",
+            "description": "Hand carved crystal pieces",
+            "image": "carvedCrystals.jpg"
+        },
+        {
+            "name": "Crystal Hearts",
+            "description": "Heart shaped crystal pieces",
+            "image": "heartCrystals.jpg"
+        },
+        {
+            "name": "Crystal Clusters",
+            "description": "Grouped crystal pieces",
+            "image": "clusterCrystals.jpg"
+        },
+    ]
+    return render_template("categories.html", crystalTypes=crystalTypes)
